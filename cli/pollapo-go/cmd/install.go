b96dcd7ac18d35fd4b93f9a5d44eca6e1a03d06a
Changseo Jang <changseo.jang@riiid.co>
2021-08-18 14:34:25 +0900

    add help

--- cli/pollapo-go/cmd/install.go
+++ cli/pollapo-go/cmd/install.go
@@ -24,13 +24,8 @@ func getCacheDir() string {
 // installCmd represents the install command
 var installCmd = &cobra.Command{
 	Use:   "install",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Install dependencies.",
+	Long: `Install dependencies.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		client := github.Client{}
 		var token, _ = cmd.Flags().GetString("token")
--- cli/pollapo-go/cmd/why.go
+++ cli/pollapo-go/cmd/why.go
@@ -24,13 +24,8 @@ import (
 // whyCmd represents the why command
 var whyCmd = &cobra.Command{
 	Use:   "why",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Show information about why dependency is installed.",
+	Long: `Show information about why dependency is installed.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("why called")
 	},
